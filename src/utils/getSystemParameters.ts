73de3b86e5847e04730f71aba5de3e4e6b7f6c4b
LayNath242 <nath.selendra@gmail.com>
2022-09-29 14:54:02 +0700

    get system parameter

---
+++ src/utils/getSystemParameters.ts
@@ -0,0 +1,28 @@
+
+import getSelendraApi from "./getSelendraApi";
+
+export const getSystemParameters = async () => {
+  const api = await getSelendraApi();
+  const params = await api.rpc.system.properties();
+
+  const decimals = !params.tokenDecimals.isNone && params.tokenDecimals.value.toHuman();
+  const symbols = 
+    (!params.tokenSymbol.isNone && (params.tokenSymbol.value.toHuman() as string[]))
+    ? (!params.tokenSymbol.isNone && (params.tokenSymbol.value.toHuman() as string[]))
+    : [""] ;
+    
+  const symbolsDecimals = symbols.reduce(
+    (acc: any, symbol: any, index: string | number) => ({
+      ...acc,
+      [symbol]: + [index],
+    }),
+    {}
+  );
+  return {
+    decimals,
+    symbols,
+    symbolsDecimals,
+  };
+};
+
+export default getSystemParameters;
